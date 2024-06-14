8ef2ec88b80e63341888026e25ba78f0d7050b87
valentinus02 <farenrajendra84@gmail.com>
2024-06-14 15:39:59 +0700

    Membuat login register dan index,serta mengatur session dan cookie

--- index.php
+++ index.php
@@ -29,18 +29,19 @@ if (isset($_GET['logout'])) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>List Suku Cadang</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta1/css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <style>
         body {
-            background:url('https://th.bing.com/th/id/OIP.tljD9WAkwObrX_SlS12V0QHaEK?rs=1&pid=ImgDetMain') no-repeat center center fixed;
+            background: url('https://www.cleverelements.com/wp-content/uploads/2024/01/warum-du-als-mechaniker-newsletter-versenden-solltest.png') no-repeat center center fixed;
             background-size: cover;
             background-color: #2c3e50;
             color: #ecf0f1;
             display: flex;
+            font-family: 'Poppins', sans-serif;
             flex-direction: column;
             min-height: 100vh;
         }
@@ -79,124 +80,207 @@ if (isset($_GET['logout'])) {
             border-color: #16a085 !important;
             color: white !important;
         }
+        .table {
+            background-color: #ecf0f1;
+            color: #2c3e50;
+            border-radius: 10px;
+            overflow: hidden;
+        }
+        .table th, .table td {
+            vertical-align: middle;
+        }
+        .table-hover tbody tr:hover {
+            background-color: #dcdde1;
+        }
+        .table thead {
+            background-color: #2c3e50;
+            color: #ecf0f1;
+        }
+        .table th {
+            border: none;
+        }
+        .table td {
+            border: none;
+            padding: 10px;
+        }
+        .btn-custom {
+            background-color: #e67e22;
+            border-color: #e67e22;
+        }
+        .btn-custom:hover {
+            background-color: #d35400;
+            border-color: #d35400;
+        }
+        .form-control, .form-select {
+            margin-right: 10px;
+        }
+        .btn-sm i {
+            margin-right: 5px;
+        }   
+        .boxed-heading {
+            background-color: rgba(236, 240, 241, 0.8); 
+            color: #2c3e50; 
+            padding: 20px; 
+            border-radius: 10px; 
+            border: 2px solid #34495e; 
+            text-align: center; 
+            display: flex; 
+            justify-content: center;
+            align-items: center; 
+            width: fit-content; 
+            margin: auto; 
+        }
+        
     </style>
+   
+    <nav class="navbar navbar-expand-lg navbar-dark position-fixed w-100">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Project Bengkel</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a id="logoutBtn" class="btn btn-danger" href="#">Logout</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container py-5 ">
+        <h1 class="text-center mb-4 boxed-heading mt-5"></i> List Suku Cadang</h1>
+        <div class="row mb-4">
+            <div class="col-md-6">
+                <form action="" method="get" class="d-flex">
+                    <input class="form-control me-2" placeholder="Cari Data" name="search"/>
+                    <select name="search_by" class="form-select me-2">
+                        <option value="">Cari Berdasarkan</option>
+                        <option value="name">Nama Barang</option>
+                        <option value="category">Category</option>
+                    </select>
+                    <button type="submit" class="btn btn-primary"><i class="bi bi-search me-2"></i> Cari</button>
+                </form>
+            </div>
+            <div class="col-md-6 text-end">
+                <a href="insert.php" class="btn btn-success"><i class="bi bi-plus-circle me-2"></i> Tambah Data</a>
+            </div>
+        </div>
+        <div class="table-responsive">
+            <table class="table table-striped table-bordered">
+                <thead class="table-dark">
+                <tr>
+                    <th>Id Barang</th>
+                    <th>Nama Barang</th>
+                    <th>Harga Barang</th>
+                    <th>Category</th>
+                    <th>Supplier</th>
+                    <th>Stock</th>
+                    <th>Tgl. Insert Data</th>
+                    <th colspan="2" class="text-center">Pilihan</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
+                date_default_timezone_set('Asia/Jakarta');
+                ini_set('display_errors', '1');
+                ini_set('display_startup_errors', '1');
+                error_reporting(E_ALL);
+                
+                require 'vendor/autoload.php';
+                 \Sentry\init([
+            'dsn' => 'https://0edc357806aff26431f2cc015401142c@o4507428341153792.ingest.us.sentry.io/4507428352032768',
+            // Specify a fixed sample rate
+            'traces_sample_rate' => 1.0,
+            // Set a sampling rate for profiling - this is relative to traces_sample_rate
+            'profiles_sample_rate' => 1.0,
+          ]);
+
+
+                require_once 'config_db.php';
+
+                $db = new ConfigDB();
+                $conn = $db->connect();
+
+                // function checkNum($number) {
+                //     if($number>1) {
+                //       throw new Exception("Value must be 1 or below");
+                //     }
+                //     return true;
+                //   }
+                // function logError($error) {
+                //     error_log($error, 3, 'error.log');
+                //  }
+                //  try {
+                //     echo checkNum(2);	
+                // } catch (Exception $e) {
+                //     logError($e->getMessage());
+                //     echo 'Error : '.$e->getMessage();
+                // }
+                    
+                // echo 'Finish';
+
+                if (isset($_GET['delete'])) {
+                    $query = $db->update('products', [
+                        'deleted_at' => date('Y-m-d H:i:s')
+                    ], $_GET['delete']);
+                }
+
+                $searchCondition = "";
+                if (isset($_GET['search']) && !empty($_GET['search'])) {
+                    $search = $conn->real_escape_string($_GET['search']);
+                    $searchBy = $_GET['search_by'];
+                    if ($searchBy == 'name') {
+                        $searchCondition = "AND a.name LIKE '%$search%'";
+                    } elseif ($searchBy == 'category') {
+                        $searchCondition = "AND b.name LIKE '%$search%'";
+                    }
+                }
+
+                $query = "SELECT a.id, a.name, a.price, a.stock, a.created_at, b.name as category_name, c.nama_supplier as supplier_name 
+                          FROM products a 
+                          LEFT JOIN categories b ON a.id_category = b.id 
+                          LEFT JOIN supplier c ON a.id_supplier = c.id_supplier 
+                          WHERE a.deleted_at IS NULL $searchCondition";
+
+                $result = $conn->query($query);
+                $totalRows = $result->num_rows;
+
+                if ($totalRows > 0) {
+                    foreach ($result as $key => $row) {
+                        echo "<tr>";
+                        echo "<td>".($key + 1)."</td>";
+                        echo "<td>".$row['name']."</td>";
+                        echo "<td>".$row['price']."</td>";
+                        echo "<td>".$row['category_name']."</td>";
+                        echo "<td>".$row['supplier_name']."</td>";
+                        echo "<td>".$row['stock']."</td>";
+                        echo "<td>".$row['created_at']."</td>";
+                        echo "<td><a class='btn btn-sm btn-primary' href='update.php?id=".$row['id']."'><i class='bi bi-pencil-square me-2'></i> Update</a></td>";
+                        echo "<td><a class='btn btn-sm btn-danger' href='index.php?delete=".$row['id']."' onclick='return confirm(\"Apakah Anda yakin ingin menghapus data?\");'><i class='bi bi-trash me-2'></i> Delete</a></td>";
+                        echo "</tr>";
+                    }
+                } else {
+                    echo "<tr><td colspan='9' class='text-center'>No Data</td></tr>";
+                }
+
+                $conn->close();
+                ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <footer class="text-center mt-5">Project by Valentinus Digmadani Nugroho & M. Faren Rajendra Ratosila</footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-U/FpXWsWM6lCRhLa0mS2nZ+5XxcSxXFImjNXIyEk1PQNLsAo69OB3O1G6YgVLk+B" crossorigin="anonymous"></script>
     <script>
-        function confirmLogout(event) {
-            event.preventDefault(); 
-            const userConfirmed = confirm("Apakah Anda yakin ingin logout?");
-            if (userConfirmed) {
-                window.location.href = event.target.href; 
+        document.getElementById('logoutBtn').addEventListener('click', function(event) {
+            if (!confirm('Apakah Anda yakin ingin logout?')) {
+                event.preventDefault();
+            } else {
+                window.location.href = 'logout.php';
             }
-        }
+        });
     </script>
-</head>
-<body>
-<nav class="navbar navbar-expand-lg navbar-dark">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">Project Bengkel</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
-                    <a class="btn btn-danger" href="logout.php" onclick="confirmLogout(event)">Logout</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
-<div class="container">
-    <h1 class="text-center mt-5 mb-4">List Suku Cadang</h1>
-    <div class="row mb-3">
-        <div class="d-flex justify-content-between align-items-center">
-            <form action="" method="get" class="d-flex align-items-center">
-                <input class="form-control" placeholder="Cari Data" name="search"/>
-                <select name="search_by" class="form-select">
-                    <option value="">Cari Berdasarkan</option>
-                    <option value="name">Nama Barang</option>
-                    <option value="category">Category</option>
-                </select>
-                <button type="submit" class="btn btn-success mx-2">Cari</button>
-            </form>
-            <a href="insert.php" class="btn btn-success btn-custom">Tambah Data <i class="fas fa-plus-circle"></i></a>
-        </div>
-    </div>
-    <table class="table table-striped table-hover">
-        <thead>
-        <tr>
-            <th>Id Barang</th>
-            <th>Nama Barang</th>
-            <th>Harga Barang</th>
-            <th>Category</th>
-            <th>Supplier</th>
-            <th>Stock</th>
-            <th>Tgl. Insert Data</th>
-            <th colspan="2">Pilihan</th>
-        </tr>
-        </thead>
-        <tbody>
-        <?php
-        date_default_timezone_set('Asia/Jakarta');
-        ini_set('display_errors', '1');
-        ini_set('display_startup_errors', '1');
-        error_reporting(E_ALL);
-
-        require_once 'config_db.php';
-
-        $db = new ConfigDB();
-        $conn = $db->connect();
-
-        if (isset($_GET['delete'])) {
-            $query = $db->update('products', [
-                'deleted_at' => date('Y-m-d H:i:s')
-            ], $_GET['delete']);
-        }
-
-        $searchCondition = "";
-        if (isset($_GET['search']) && !empty($_GET['search'])) {
-            $search = $conn->real_escape_string($_GET['search']);
-            $searchBy = $_GET['search_by'];
-            if ($searchBy == 'name') {
-                $searchCondition = "AND a.name LIKE '%$search%'";
-            } elseif ($searchBy == 'category') {
-                $searchCondition = "AND b.name LIKE '%$search%'";
-            }
-        }
-
-        $query = "SELECT a.id, a.name, a.price, a.stock, a.created_at, b.name as category_name, c.nama_supplier as supplier_name 
-                  FROM products a 
-                  LEFT JOIN categories b ON a.id_category = b.id 
-                  LEFT JOIN supplier c ON a.id_supplier = c.id_supplier 
-                  WHERE a.deleted_at IS NULL $searchCondition";
-
-        $result = $conn->query($query);
-        $totalRows = $result->num_rows;
-
-        if ($totalRows > 0) {
-            foreach ($result as $key => $row) {
-                echo "<tr>";
-                echo "<td>".($key + 1)."</td>";
-                echo "<td>".$row['name']."</td>";
-                echo "<td>".$row['price']."</td>";
-                echo "<td>".$row['category_name']."</td>";
-                echo "<td>".$row['supplier_name']."</td>";
-                echo "<td>".$row['stock']."</td>";
-                echo "<td>".$row['created_at']."</td>";
-                echo "<td><a class='btn btn-sm btn-info' href='update.php?id=".$row['id']."'><i class='fas fa-edit'></i> Update</a></td>";
-                echo "<td><a class='btn btn-sm btn-danger' href='index.php?delete=".$row['id']."' onclick='return confirm(\"Apakah Anda yakin ingin menghapus data?\")'><i class='fas fa-trash-alt'></i> Delete</a></td>";
-                echo "</tr>";
-            }
-        } else {
-            echo "<tr><td colspan='8' class='text-center'>No Data</td></tr>";
-        }
-
-        $conn->close();
-        ?>
-        </tbody>
-    </table>
-</div>
-<footer class="text-center">Project by Valentinus Digmadani Nugroho & M. Faren Rajendra Ratosila</footer>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+MDJtIWYgvAbH9M56j8e0NeqN90B4" crossorigin="anonymous"></script>
 </body>
 </html>
